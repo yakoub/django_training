6c472cd51aa9a9e853a59fa793264c2940ccdedd
yakoub abaya <sting606@gmail.com>
2014-11-02 00:17:51 +0200

    generic model forms require form action context

--- first/urls.py
+++ first/urls.py
@@ -5,7 +5,7 @@ from first import views
 urlpatterns = patterns('',
   url(r'first/$', views.FirstList.as_view(), name='first-list'),
   url(r'first/(?P<pk>\d+)/$', views.FirstView.as_view(), name='first-view'),
-  url(r'first/create/$', views.FirstCreate.as_view()),
+  url(r'first/create/$', views.FirstCreate.as_view(), name='first-create'),
   url(r'first/(?P<pk>\d+)/edit$', views.FirstUpdate.as_view()),
   url(r'first/(?P<pk>\d+)/delete$', views.FirstDelete.as_view()),
 )
--- first/views.py
+++ first/views.py
@@ -3,21 +3,42 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView, ListView
 from django.core.urlresolvers import reverse_lazy
 from first.models import First
+import logging
+logger = logging.getLogger('django')
 
 class FirstCreate(CreateView):
   model = First
   fields = ['title', 'body']
 
+  def get_context_data(self, **kwargs):
+    context = super(FirstCreate, self).get_context_data(**kwargs)
+    context['action'] = reverse_lazy('first-create') 
+    return context
+
 class FirstUpdate(UpdateView):
   model = First
   fields = ['title', 'body']
+
+  def get_context_data(self, **kwargs):
+    context = super(FirstUpdate, self).get_context_data(**kwargs)
+    context['action'] = reverse_lazy('first-view', kwargs={'pk': self.object.pk}) + '/edit'
+    return context
 
 class FirstDelete(DeleteView):
   model = First
   success_url = reverse_lazy('first-list')
 
+  def get_context_data(self, **kwargs):
+    context = super(FirstDelete, self).get_context_data(**kwargs)
+    context['action'] = reverse_lazy('first-view', kwargs={'pk': self.object.pk}) + '/delete'
+    return context
+
 class FirstView(DetailView):
   model = First
+
+  def get(self, request, *args, **kwargs):
+    logger.error('django view')
+    return super(FirstView, self).get(request, args, kwargs)
 
 class FirstList(ListView):
   model = First
