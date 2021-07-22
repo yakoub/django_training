e9143d7d8f1536bc904e92bfb5df5ebcbd5db10b
yakoub abaya <sting606@gmail.com>
2021-07-22 08:08:06 +0300

    cascade delete

--- article/models.py
+++ article/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Paragraph(models.Model):
   content = models.TextField()
   ordinal = models.IntegerField()
-  article = models.ForeignKey('Article')
+  article = models.ForeignKey('Article', models.CASCADE)
 
   def get_absolute_url(self):
     return reverse('articles:list') + '#content/{0}/{1}'.format(self.article.pk, self.pk)
